c9b76de52e6ae9d4a84d2c5481dacedf3257d0d0
DESKTOP-Q4B6B51\JITHIN KM <jithinmuralikm@gmail.com>
2024-12-05 16:24:08 +0530

    configurations added

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -17,3 +17,27 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.get('/', (req, res) => {
+    return res.status(200).send("Hiiiiii");
+});
+
+exports.app = functions.https.onRequest(app);
