20959d135f0d12d466e95ecea89515b2106deab2
DESKTOP-Q4B6B51\JITHIN KM <jithinmuralikm@gmail.com>
2024-12-05 17:58:59 +0530

    new changes

--- functions/index.js
+++ functions/index.js
@@ -36,8 +36,67 @@ const app = express();
 
 app.use(cors({ origin: true }));
 
+const db = admin.firestore();
+
 app.get('/', (req, res) => {
     return res.status(200).send("Hiiiiii");
 });
+
+app.post('/api/create', (req, res) => {
+    (async () => {
+        try {
+            await db.collection("orders").doc(`/${Date.now()}/`).create({
+                id: Date.now(),
+                name: req.body.name,
+                mobile: req.body.mobile,
+                address: req.body.address
+            });
+            return res.status(200).send({ status: "Success", msg: "Data saved" });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({ status: "Failed", msg: error });
+        }
+    })();
+});
+
+app.get('/api/get/:id', (req, res) => {
+    (async () => {
+        try {
+            const reqDoc = db.collection('orders').doc(req.params.id)
+            let orderDetail = await reqDoc.get();
+            let response = orderDetail.data();
+            return res.status(200).send({ status: "Success", data: response })
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({ status: "Failed", msg: error });
+        }
+    })();
+})
+
+app.get('/api/getAll', (req, res) => {
+    (async () => {
+        try {
+            const query = db.collection('orders');
+            let response = [];
+
+            await query.get().then((data) => {
+                let docs = data.docs;
+                docs.map((doc) => {
+                    const selectedItem = {
+                        name: doc.data().name,
+                        mobile: doc.data().mobile,
+                        address: doc.data().address
+                    };
+                    response.push(selectedItem);
+                });
+                return response
+            });
+            return res.status(200).send({ status: "Success", data: response })
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({ status: "Failed", msg: error });
+        }
+    })();
+})
 
 exports.app = functions.https.onRequest(app);
